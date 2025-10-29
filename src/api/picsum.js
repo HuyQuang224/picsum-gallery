96e1ee4a2243c977342253e64086bed0f297a4b3
HuyQuang224 <huyquang222004@gmail.com>
2025-10-29 23:48:51 +0700

    Fix api url

--- src/api/picsum.js
+++ src/api/picsum.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 const BASE_URL = 'https://picsum.photos';
 
 export const fetchPhotos = async (page = 1, limit = 20) => {
-  const res = await axios.get(`${BASE_URL}/list?page=${page}&limit=${limit}`);
+  const res = await axios.get(`${BASE_URL}/v2/list?page=${page}&limit=${limit}`);
   return res.data;
 };
